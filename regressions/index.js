c68db381f5f00791363e6f271323ffa927b716fd
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-14 02:26:30 +0300

    Loading Data

---
+++ regressions/index.js
@@ -0,0 +1,13 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+
+const loadCSV = require('./load-csv');
+
+const { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ['horsepower'],
+  labelColumns: ['mpg'],
+});
+
+console.log(features);
