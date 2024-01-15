930547b15dda997f9d7ca8c53bdaa0b501203b8a
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-15 05:12:15 +0300

    Initial Gradient Descent Implementation

--- regressions/index.js
+++ regressions/index.js
@@ -2,6 +2,7 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 
 const loadCSV = require('./load-csv');
+const LinearRegression = require('./linear-regression');
 
 const { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   shuffle: true,
@@ -10,4 +11,12 @@ const { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   labelColumns: ['mpg'],
 });
 
-console.log(features);
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.001,
+  iterations: 100,
+});
+
+regression.train();
+
+console.log('Updated M is ', regression.m);
+console.log('Updated B is ', regression.b);
